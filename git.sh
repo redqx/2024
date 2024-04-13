76e9237c2c6afa9cd0f96f1928229e8d7615be59
redqx <tinyxiangxiang@outlook.com>
2024-04-13 08:05:28 +0800

    日常更新

---
+++ git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 切换到你的代码仓库目录
+#cd /path/to/your/repository
+
+# 执行 git add
+git add .
+
+# 执行 git commit，并使用当前日期作为提交信息
+git commit -m "日常更新"
+
+# 执行 git push
+git push origin master
